14b039e02fdf16a4bb3b665cb76256766a06d7a9
andreadegiovine <68383596+andreadegiovine@users.noreply.github.com>
2025-12-10 08:19:27 +0100

    Revert "Migrate to browserless.io 5"
    This reverts commit 0c934d56b2e073310c07d1545c5bb985b23bf523.

--- src/index.js
+++ src/index.js
@@ -108,11 +108,7 @@ export default {
         })
       });
 
-      const text = await response.text();
-      console.log("Browserless raw response:", text);
-
-      // const result = await response.json();
-      result = JSON.parse(text);
+      const result = await response.json();
 
       if (result.success && result.code) {
         return new Response(JSON.stringify({ code: result.code }), {
